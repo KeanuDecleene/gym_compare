2761dc83e97ff58b9f65a3043204968b1d9e1a9d
KeanuDecleene <keanudecleene124@gmail.com>
2025-11-22 22:44:19 +1300

    rm imports

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
+from PyQt6.QtWidgets import QWidget
 from PyQt6.QtCore import Qt
 from gui.main_window_setup import GymCompareSetup
 import requests
